95e3df07783ed969ac617dcf069eea52d6df112d
arorachirag <arorachirag52@gmail.com>
2023-11-15 13:10:38 +0530

    Code changes1

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -15,7 +15,7 @@ package com.driver;
     public class Main {
 
         public static void main(String[] args) {
-            B b = new B();
+            A b = new B();
             System.out.println(b.meth());
         }
 
